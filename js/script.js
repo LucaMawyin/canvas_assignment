60777737f0c421b90010ae1271e7131df2ac02e5
Luca Mawyin <lucamawyin@gmail.com>
2025-02-27 14:05:36 -0500

    Added position function

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         button.addEventListener('click', () => {
             draw(button.id)
         });
+
     });
 
 
@@ -18,27 +19,5 @@ document.addEventListener('DOMContentLoaded', () => {
                 context.clearRect(0, 0, canvas.width, canvas.height);
                 break;
 
-        }
-    }
-});
-class Triangle {
-    constructor(sideLength, color) {
-        this.sideLength = sideLength;
-        this.color = color;
-    }
-
-}
-
-class Circle {
-    constructor(radius, color) {
-        this.radius = radius;
-        this.color = color;
-    }
-}
-
-class Square {
-    constructor(width, color) {
-        this.width = width;
-        this.color = color;
     }
 }
