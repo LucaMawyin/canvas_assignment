2de79f09a3b9f13aaf942326dab5c67421349187
Neelan <neelan2317@gmail.com>
2025-02-27 13:52:55 -0500

    Add class definitions for shapes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+
+    let canvas = document.getElementById("canvas");
 
 
 });
+
+
+
+class Triangle {
+    constructor(sideLength, color) {
+        this.sideLength = sideLength;
+        this.color = color;
+    }
+
+}
+
+class Circle {
+    constructor(radius, color) {
+        this.radius = radius;
+        this.color = color;
+    }
+}
+
+class Square {
+    constructor(width, color) {
+        this.width = width;
+        this.color = color;
+    }
+}
