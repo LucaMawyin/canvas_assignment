ec2bddce58a6b7e41902254a30be5057cb25a391
Luca Mawyin <lucamawyin@gmail.com>
2025-02-27 13:52:13 -0500

    Started draw function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     
 
+    document.querySelectorAll('body button').forEach(button => {
 
+        button.addEventListener('click', () => {
+            draw(button.id)
+        });
+
+    });
 });
+
+function draw(shape){
+    switch (shape.toLowerCase()){
+        case "reset":
+            const context = canvas.getContext('2d');
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            break;
+
+    }
+}
